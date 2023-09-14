d6242ebb21a3d3e04e4bb9389070231276524dbc
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-09-14 15:01:40 +0400

    add format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
