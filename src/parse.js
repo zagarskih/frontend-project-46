c40fa2c14621fb6c8697f76aa7f71be65a24bb5e
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-09-28 01:12:48 +0400

    rename parse -> parsers

--- src/parse.js
+++
@@ -1,9 +0,0 @@
-const getFileParse = (fileData, fileType) => {
-  let fileParse;
-  if (fileType === 'json') {
-    fileParse = JSON.parse(fileData);
-  }
-  return fileParse;
-};
-
-export default getFileParse;
