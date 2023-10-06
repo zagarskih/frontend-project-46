7ed79fcb2259fde1817601aa4120a757b0d3672e
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-10-06 18:11:24 +0400

    update

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,39 +1,29 @@
+import _ from 'lodash';
+
 const makeTree = (data1, data2) => {
-  const keys2 = Object.keys(data2);
   const keys1 = Object.keys(data1);
-  const diffs1 = keys2.reduce((acc, key) => {
-    if (!Object.hasOwn(data1, key)) {
-      acc.push(['+', key, data2[key]]);
-    } else if (Object.hasOwn(data1, key) && data1[key] !== data2[key]) {
-      acc.push(['-', key, data1[key]]);
-      acc.push(['+', key, data2[key]]);
-    } else if (Object.hasOwn(data1, key) && data1[key] === data2[key]) {
-      acc.push([' ', key, data1[key]]);
-    }
-    return acc;
-  }, []);
+  const keys2 = Object.keys(data2);
+  const unsortedKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(unsortedKeys);
 
-  const diffs2 = keys1.reduce((acc, key) => {
-    if (!Object.hasOwn(data2, key)) {
-      acc.push(['-', key, data1[key]]);
-    }
-    return acc;
-  }, []);
-
-  const diffs = [...diffs1, ...diffs2];
-  const sortedDiffs = diffs.sort((arr1, arr2) => {
-    if (arr1[1] < arr2[1]) {
-      return -1;
-    }
-    if (arr1[1] > arr2[1]) {
-      return 1;
-    }
-    return 0;
-  });
-
-  const strOfDiffs = sortedDiffs.map((elem) => `  ${elem[0]} ${elem[1]}: ${elem[2]}`).join('\n');
-  const tree = `{\n${strOfDiffs}\n}`;
-  return tree;
+  return sortedKeys
+    .map((key) => {
+      if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+        return { key, children: makeTree(data1[key], data2[key]), status: 'nested' };
+      }
+      if (!Object.hasOwn(data1, key)) {
+        return { key, value: data2[key], status: 'added' };
+      }
+      if (!Object.hasOwn(data2, key)) {
+        return { key, value: data1[key], status: 'removed' };
+      }
+      if (data1[key] === data2[key]) {
+        return { key, value: data1[key], status: 'unupdated' };
+      }
+      return {
+        key, oldValue: data1[key], newValue: data2[key], status: 'updated',
+      };
+    });
 };
 
 export default makeTree;
