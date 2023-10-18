d07a1905c1fe5ed021acaf46897a8f70bcb3bf2e
Ekaterina Zagarskikh <zagarskihkate@gmail.com>
2023-10-18 22:06:48 +0400

    update import genDiff

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import genDiff from '../src/index.js';
+import genDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
